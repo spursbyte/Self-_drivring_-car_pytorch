73001c6d0ad0221b5e93ad88f535e6d63c0c8121
Siddharth Mohanty <60615807+spursbyte@users.noreply.github.com>
2020-10-12 10:06:28 +0530

    Update track.py

--- Self_Driving_Car/track.py
+++ Self_Driving_Car/track.py
@@ -1,12 +1,12 @@
 # Self Driving Car
 
-# Importing the libraries
+
 import numpy as np
 from random import random, randint
 import matplotlib.pyplot as plt
 import time
 
-# Importing the Kivy packages
+
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
@@ -16,25 +16,24 @@ from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProper
 from kivy.vector import Vector
 from kivy.clock import Clock
 
-# Importing the Dqn object from our AI in ai.py
+
 from ai import Dqn
 
-# Adding this line if we don't want the right click to put a red point
+
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 
-# Introducing last_x and last_y, used to keep the last point in memory when we draw the sand on the map
+
 last_x = 0
 last_y = 0
 n_points = 0
 length = 0
 
-# Getting our AI, which we call "brain", and that contains our neural network that represents our Q-function
+
 brain = Dqn(5,3,0.9)
 action2rotation = [0,20,-20]
 last_reward = 0
 scores = []
 
-# Initializing the map
 first_update = True
 def init():
     global sand
@@ -46,10 +45,7 @@ def init():
     goal_y = largeur - 20
     first_update = False
 
-# Initializing the last distance
 last_distance = 0
-
-# Creating the car class
 
 class Car(Widget):
     
